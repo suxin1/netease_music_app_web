8b088e983e9f9c6fbb8cd67cd452cb10695f91c0
何书鑫 <heshuxin@1m2.net>
2020-09-10 09:54:10 +0800

    引入core-js：js新特性支持

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "core-js";  // polyfills
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
